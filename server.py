349c73afbbeda3b9387f17120e3c1aff49760c16
Jacob Boober <Bjacobwork@gmail.com>
2021-04-20 01:21:26 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,17 +1,27 @@
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
+import os
 
 app = Flask(__name__)
 
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
     """Respond to incoming calls with a simple text message."""
+
+    # Use this data in your application logic
+    from_number = request.form['From']
+    to_number = request.form['To']
+    body = request.form['Body']
+    print(from_number)
+    print(to_number)
+    print(body)
+
     # Start our TwiML response
     resp = MessagingResponse()
 
     # Add a message
-    resp.message("Did it work?")
+    resp.message("Did this work?")
 
     return str(resp)
 
-app.run(debug=True, port=80, host="104.236.229.86")
+app.run(debug=True, port=80, host=os.environ["DB_HOST"])
